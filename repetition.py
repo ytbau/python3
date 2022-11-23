ffd997279d0d1d93c7a02003d84d1c739b3febda
ytbau <19955805+ytbau@users.noreply.github.com>
2022-11-23 13:14:45 +0000

    message added file repetition.py

---
+++ repetition.py
@@ -0,0 +1,15 @@
+'''
+block comments
+'''
+
+# repetition using while
+i = 5
+while i > 0:
+    print(i, end=" ")
+    i = i - 1
+print("Blastoff!")
+
+# repetition using for
+for i in range(5, 0, -1):
+    print(i, end=" ")
+print("Blastoff!")
