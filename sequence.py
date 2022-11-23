15af6d7e648ccd868b76a2a1cd7b2dd77c24c172
ytbau <19955805+ytbau@users.noreply.github.com>
2022-11-23 13:05:28 +0000

    message added sequence.py file

--- sequence.py
+++ sequence.py
@@ -1,1 +1,6 @@
-integer1 = int(input
+integer1 = int(input("Enter first integer: "))
+integer2 = int(input("Enter second integer: "))
+
+integer3 = integer1 + integer2
+
+print(f"{integer1} + {integer2} = {integer3}")
